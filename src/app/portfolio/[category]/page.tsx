f4be5012fc5a980dc1e625028e79924d3b59da91
exeibier <xabierhh@gmail.com>
2025-10-17 15:20:17 -0600

    updated documental photos

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -34,7 +34,7 @@ interface Photo {
       alt: `dj Photography ${id}`
     };
   });
-  const documentalImages = Array.from({ length: 68 }, (_, i) => {
+  const documentalImages = Array.from({ length: 75 }, (_, i) => {
     const id = (i + 1).toString();
     return {
       id,
